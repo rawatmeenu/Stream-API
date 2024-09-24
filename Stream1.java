95c82cc17f8f7132d190e7bb2c9176d76d45b77a
rawatmeenu <rawatmeenu2000@gmail.com>
2024-09-24 20:22:31 +0530

    added files

---
+++ Stream1.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Stream;
+
+public class Stream1 {
+    public static void main(String args[])
+    {
+        List<String> dept = new ArrayList<>();
+        dept.add("HR");
+        dept.add("sales");
+        dept.add("computer science");
+        dept.add("Supply");
+        dept.add("Marketing");
+
+        Stream<String> depstream = dept.stream();
+        depstream.forEach(System.out::println);
+
+        System.out.println("*********************");
+
+        Stream<String> instream = Stream.of("Easy", "java", "oevrriding");
+        instream.forEach(System.out::println);
+
+        System.out.println("*********************");
+
+        Stream<String> parastream = dept.parallelStream();
+        parastream.forEach(System.out::println);
+
+        System.out.println("*********************");
+
+        Stream<String> emp= Stream.empty();
+        emp.forEach(System.out::println);
+
+        System.out.println("*********************");
+
+        Stream<String> mapdep= dept.stream();
+        //mapdep.map(wor -> wor.toUpperCase()).forEach(System.out::println);
+
+        dept.stream().map(word -> word.toUpperCase()).forEach(System.out::println);
+
+        //FLATMAP
+        String[] arrwords = {"first", "second", "third", "fourth"," fifth"};
+        Stream<String> strwords= Arrays.stream(arrwords);
+        Stream<String[]> arr2words= strwords.map(word -> word.split(" "));
+        arr2words.flatMap(Arrays::stream).forEach(System.out::println);
+
+        System.out.println("**************************");
+        //FILTER
+        dept.stream().filter(word -> word.startsWith("S")).forEach(System.out::println);
+
+        //LIMIT
+        Stream.generate(new Random()::nextInt).limit(10).forEach(System.out::println);
+
+        //SKIP: skip first 5 values and limit upto 10th number
+        Stream.iterate(1, n-> n+1).limit(10).skip(5).forEach(System.out::println);
+
+
+
+
+    }
+}
